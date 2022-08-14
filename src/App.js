583dc5bee9122495a006c7d55d6039ba5f7c85b3
R. Fariz <farizramazanov38@gmail.com>
2022-08-14 17:07:40 +0400

    feat: make Footer component responsive

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ export default class App extends Component {
           onFilterProjects={this.onFilterProjects}
         />
         <Testimonial />
-        {/* <Footer /> */}
+        <Footer />
       </>
     );
   }
